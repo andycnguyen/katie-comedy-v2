956db96e7e6c6501d9b8e0997a9cc3ed41c7cb23
Andy Nguyen <andynguyen@trailwayscounseling.onmicrosoft.com>
2025-01-07 23:07:22 -0500

    layout rework

--- KatieComedy/Pages/Admin/Photos/Edit.cshtml.cs
+++ KatieComedy/Pages/Admin/Photos/Edit.cshtml.cs
@@ -4,15 +4,42 @@ namespace KatieComedy.Web.Pages.Admin.Photos;
 
 public class EditModel(PhotoService service) : PageModel
 {
-    public Photo Photo { get; set; }
+    [FromRoute]
+    public int Id { get; set; }
 
-    public async Task OnGetAsync(int id)
+    [BindProperty, DataType(DataType.Date)]
+    public DateOnly? Date { get; set; }
+
+    [BindProperty, MaxLength(500)]
+    public string? Caption { get; set; }
+
+    [BindProperty, MaxLength(500)]
+    public string? Credit { get; set; }
+
+    public async Task OnGet()
     {
-        Photo = await service.Get(id);
+        var photo = await service.Get(Id);
+
+        Date = photo.Date;
+        Caption = photo.Caption;
+        Credit = photo.Credit;
     }
 
     public async Task<IActionResult> OnPostAsync()
     {
-        return Page();
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        await service.Update(new PhotoUpdate
+        {
+            Id = Id,
+            Date = Date,
+            Caption = Caption,
+            Credit = Credit
+        });
+
+        return RedirectToPage("Index");
     }
 }
