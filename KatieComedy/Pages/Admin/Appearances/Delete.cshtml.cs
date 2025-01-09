0182e7aa91918ed9acf03c1010c0d6aba8a731d5
Andy Nguyen <andynguyen@trailwayscounseling.onmicrosoft.com>
2025-01-08 20:39:21 -0500

    add new page scaffolds

---
+++ KatieComedy/Pages/Admin/Appearances/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KatieComedy.Web.Pages.Admin.Appearances
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KatieComedy/Pages/Admin/Biography/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KatieComedy.Web.Pages.Admin.Biography
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
