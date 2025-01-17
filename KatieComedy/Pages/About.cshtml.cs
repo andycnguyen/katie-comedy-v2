867c26a36dda6457ea294ce5c0192fde2d554865
Andy Nguyen <andynguyen@trailwayscounseling.onmicrosoft.com>
2025-01-17 07:09:02 -0500

    rename field

--- KatieComedy/Pages/About.cshtml.cs
+++ KatieComedy/Pages/About.cshtml.cs
@@ -6,7 +6,7 @@ public class AboutModel(BiographyService service) : PageModel
 {
     public string Text { get; set; } = string.Empty;
 
-    public string HtmlText => BiographyService.FormatAsHtml(Text);
+    public string Html => BiographyService.FormatAsHtml(Text);
 
     public async Task OnGet()
     {
