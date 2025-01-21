8a657beb239c75c277a018569e460463fba3aedb
Andy Nguyen <andynguyen@trailwayscounseling.onmicrosoft.com>
2025-01-21 07:50:54 -0500

    add appearance import

---
+++ KatieComedy.App.Test/AppearanceImport.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using Microsoft.EntityFrameworkCore;
+using CsvHelper;
+using KatieComedy.App.Database;
+
+namespace KatieComedy.App.Test;
+
+public class AppearanceImport
+{
+    [Fact]
+    public async Task ImportAppearances()
+    {
+        var newAppearances = GetAppearances();
+
+        var connectionstring = "";
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer(connectionstring);
+        using var dbContext = new ApplicationDbContext(optionsBuilder.Options);
+        var currentAppearances = await dbContext.Appearances.ToListAsync();
+
+        var appearancesToAdd = newAppearances
+            .Where(x => !currentAppearances.Any(y => y.DateTime == x.datetime))
+            .ToList();
+
+        dbContext.Appearances.AddRange(newAppearances.Select(x => new Database.Appearance
+        {
+            DateTime = x.datetime,
+            LocationName = x.venue_name,
+            LocationUrl = x.venue_url,
+            EventName = x.event_name,
+            EventUrl = x.event_url
+        }));
+
+        await dbContext.SaveChangesAsync();
+    }
+
+    private static IReadOnlyList<Appearance> GetAppearances()
+    {
+        using var reader = new StreamReader("v1-appearances.csv");
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        var records = csv.GetRecords<Appearance>();
+        return records.ToList();
+    }
+
+    public record Appearance(
+        int id,
+        DateTime datetime,
+        string venue_name,
+        string? venue_url,
+        string event_name,
+        string event_url);
+}
--- KatieComedy.App.Test/UrlTests.cs
+++ KatieComedy.App.Test/UrlTests.cs
@@ -6,15 +6,18 @@ public partial class UrlTests
 {
     [Theory]
     [InlineData("https://nytimes.com", true)]
+    [InlineData("http://nytimes.com", true)]
     [InlineData("nytimes.com", true)]
+    [InlineData("https://subdomain.nytimes.com/news/article", true)]
     [InlineData("https://nytimes.com/news/article", true)]
     [InlineData("https://nytimes.com/news/article-abc-123", true)]
     [InlineData("https://nytimes.com/news/article/", true)]
     [InlineData("https://nytimes.com/news/article/story/scoop", true)]
+    [InlineData("nytimes", false)]
     [InlineData("https://nytimes.", false)]
     [InlineData("htp://nytimes.com", false)]
     [InlineData("://nytimes.", false)]
-    public void Foo(string input, bool isValid)
+    public void UrlRegex_WhenMatchesInput_ValidatesUrl(string input, bool isValid)
     {
         var match = UrlRegex().Match(input);
         Assert.Equal(isValid, match.Success);
